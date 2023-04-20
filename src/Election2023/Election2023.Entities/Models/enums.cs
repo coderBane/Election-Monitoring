5dd27453aba859a54e6d98369d9137c98bb28b53
coderBane <110666273+coderBane@users.noreply.github.com>
2023-04-20 16:07:17 +0100

    refactor - corrected spelling of 21

--- src/Election2023/Election2023.Entities/Models/enums.cs
+++ src/Election2023/Election2023.Entities/Models/enums.cs
@@ -3,7 +3,7 @@
     public enum StateName
     {
         Abia = 1, Adamawa, AkwaIbom, Anambra, Bauchi, Bayelsa, Benue, Borno, CrossRiver, Delta,
-        Ebonyi, Edo, Ekiti, Enugu, FCT, Gombe, Imo, Jigawa, Kaduna, Kano, Kastina, Kebbi, Kogi,
+        Ebonyi, Edo, Ekiti, Enugu, FCT, Gombe, Imo, Jigawa, Kaduna, Kano, Katsina, Kebbi, Kogi,
         Kwara, Lagos, Nasarawa, Niger, Ogun, Ondo, Osun, Oyo, Plateau, Rivers, Sokoto, Taraba, Yobe, Zamfara
     }
 

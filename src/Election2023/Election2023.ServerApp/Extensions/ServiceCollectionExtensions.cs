93a3cac990d651c595f1a3b654fcf4f578ae9813
coderBane <110666273+coderBane@users.noreply.github.com>
2023-05-11 16:18:42 +0100

    add - current user service implementation

---
+++ src/Election2023/Election2023.ServerApp/Extensions/ServiceCollectionExtensions.cs
@@ -0,0 +1,18 @@
+﻿using Election2023.ServerApp.Services;
+using Election2023.Application.Interfaces.Services;
+
+using Microsoft.AspNetCore.Components.Server.Circuits;
+using Microsoft.Extensions.DependencyInjection.Extensions;
+
+namespace Election2023.ServerApp.Extensions;
+
+internal static class ServiceCollectionExtensions
+{
+	internal static IServiceCollection AddCurrentUserService(this IServiceCollection services)
+	{
+        services.AddScoped<ICurrentUserService, CurrentUserService>();
+		services.TryAddEnumerable(ServiceDescriptor.Scoped<CircuitHandler, UserCircuitHandler>());
+        return services;
+	}
+}
+

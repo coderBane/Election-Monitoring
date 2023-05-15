37ce65d049a28ea918f303671cbe58fbf2895e8b
coderBane <110666273+coderBane@users.noreply.github.com>
2023-05-15 06:36:56 +0100

    refactor - interface global usings

--- src/Election2023/Election2023.DataStore/Usings.cs
+++ src/Election2023/Election2023.DataStore/Usings.cs
@@ -4,3 +4,6 @@ global using Election2023.Domain.Enums;
 global using Election2023.Domain.Models.Voting;
 global using Election2023.Domain.Models.Candidacy;
 
+global using Election2023.Application.Interfaces.Services;
+global using Election2023.Application.Interfaces.Repositories;
+

08030ece66085ae2a4c9c39823e6c92e1f65905a
coderBane <110666273+coderBane@users.noreply.github.com>
2023-05-15 18:56:53 +0100

    refactor - add seed method for candiddates

--- src/Election2023/Election2023.DataStore/Database/ElectionDbContextExtensions.cs
+++ src/Election2023/Election2023.DataStore/Database/ElectionDbContextExtensions.cs
@@ -1,4 +1,5 @@
 using System.Text.Json;
+using Election2023.DataStore.Converters.JsonConverters;
 using Election2023.Application.VeiwModels.Incoming;
 
 namespace Election2023.DataStore.Database;
@@ -12,6 +13,7 @@ public static class ElectionDbContextExtensions
     private static readonly List<District> Districts = new();
     private static readonly List<FederalConstituency> FCs = new();
     private static readonly List<Election> Elections = new();
+    private static List<Candidate> Candidates = new();
     private static PoliticalParty[] PoliticalParties = Array.Empty<PoliticalParty>();
 
     public static void Initialize(this ElectionDbContext context, string path)
@@ -19,6 +21,7 @@ public static class ElectionDbContextExtensions
         if (context is null) throw new ArgumentNullException(nameof(context));
 
         context.SeedParties();
+        context.SeedCandidates(path);
 
         // context.SeedStates(path);
         // context.SeedDistrict(path);
@@ -30,27 +33,55 @@ public static class ElectionDbContextExtensions
     public static void DbCleanUp(this ElectionDbContext context)
     {
         // context.States.ExecuteDelete();
-        context.PoliticalParties.ExecuteDelete();
+        // context.PoliticalParties.ExecuteDelete();
     }
 
     public static void SeedParties(this ElectionDbContext context)
     {
         if (context.PoliticalParties.Any()) return;
 
-        Array.Resize(ref PoliticalParties, 10);
+        Array.Resize(ref PoliticalParties, 18);
 
         PoliticalParties[0] = new() { Abbrv = Party.A, Name = "Accord", Colour = "SlateGray" };
-        PoliticalParties[1] = new() { Abbrv = Party.LP, Name = "Labour Party", Colour = "LimeGreen" };
-        PoliticalParties[2] = new() { Abbrv = Party.APC, Name = "All Progressive Congress", Colour = "DeepSkyBlue" };
-        PoliticalParties[3] = new() { Abbrv = Party.PDP, Name = "People's Democratic Party", Colour = "Red" };
-        PoliticalParties[4] = new() { Abbrv = Party.SDP, Name = "Social Democratic Party", Colour = "Orange" };
-        PoliticalParties[5] = new() { Abbrv = Party.NNPP, Name = "New Nigeria People's Party", Colour = "MediumBlue" };
+        PoliticalParties[1] = new() { Abbrv = Party.AA, Name = "Action Alliance", Colour = "CornflowerBlue" };
+        PoliticalParties[2] = new() { Abbrv = Party.AAC, Name = "African Action Congress", Colour = "Cornsilk" };
+        PoliticalParties[3] = new() { Abbrv = Party.ADC, Name = "African Democratic Congress", Colour = "DarkGreen" };
+        PoliticalParties[4] = new() { Abbrv = Party.ADP, Name = "Action Democratic Party", Colour = "Navy" };
+        PoliticalParties[5] = new() { Abbrv = Party.APC, Name = "All Progressive Congress", Colour = "DeepSkyBlue" };
         PoliticalParties[6] = new() { Abbrv = Party.APGA, Name = "All Progressive Grand Allaiance", Colour = "Yellow" };
-        PoliticalParties[7] = new() { Abbrv = Party.ADC, Name = "African Democratic Congress", Colour = "DarkGreen" };
-        PoliticalParties[8] = new() { Abbrv = Party.ZLP, Name = "Zenith Labour Party", Colour = "Black" };
-        PoliticalParties[9] = new() { Abbrv = Party.NRM, Name = "National Rescue Movement", Colour = "Gold" };
+        PoliticalParties[7] = new() { Abbrv = Party.APM, Name = "Allied Peoples Movement", Colour = "MediumPurple" };
+        PoliticalParties[8] = new() { Abbrv = Party.APP, Name = "Action Peoples Party", Colour = "Tomato" };
+        PoliticalParties[9] = new() { Abbrv = Party.BP, Name = "Boot Party", Colour = "LightGreen" };
+        PoliticalParties[10] = new() { Abbrv = Party.LP, Name = "Labour Party", Colour = "LimeGreen" };
+        PoliticalParties[11] = new() { Abbrv = Party.NNPP, Name = "New Nigeria People's Party", Colour = "MediumBlue" };
+        PoliticalParties[12] = new() { Abbrv = Party.NRM, Name = "National Rescue Movement", Colour = "Gold" };
+        PoliticalParties[13] = new() { Abbrv = Party.PDP, Name = "People's Democratic Party", Colour = "Red" };
+        PoliticalParties[14] = new() { Abbrv = Party.PRP, Name = "People's Redemption Party", Colour = "FireBrick" };
+        PoliticalParties[15] = new() { Abbrv = Party.SDP, Name = "Social Democratic Party", Colour = "Orange" };
+        PoliticalParties[16] = new() { Abbrv = Party.YPP, Name = "Young Progressive Party", Colour = "Goldenrod" };
+        PoliticalParties[17] = new() { Abbrv = Party.ZLP, Name = "Zenith Labour Party", Colour = "Black" };
 
         context.PoliticalParties.AddRange(PoliticalParties);
+    }
+
+    public static void SeedCandidates(this ElectionDbContext context, string path)
+    {
+        var json = GetJson(path, "candidates.json");
+        if (json == null) return;
+
+        var options = new JsonSerializerOptions();
+        options.Converters.Add(new StringEnumJsonConverter<Party>());
+        options.Converters.Add(new StringEnumJsonConverter<Gender>());
+        options.Converters.Add(new StringEnumJsonConverter<ElectionType>());
+
+        context.Candidates.ExecuteDelete();
+
+        if(!context.Candidates.Any(x => x.Category == ElectionType.Presidential))
+        {
+            Candidates = JsonSerializer.Deserialize<List<Candidate>>(json, options) ?? new List<Candidate>();
+            if (Candidates.Any())
+                context.AddRange(Candidates);
+        }
     }
 
     // private static void SeedStates(this ElectionDbContext context, string path)
@@ -181,18 +212,18 @@ public static class ElectionDbContextExtensions
     // private static State? ParseState(string state)
         // => States.FirstOrDefault(x => x.Name == Enum.Parse<StateName>(state));
 
-    // private static string? GetJson(string contentPath, string file)
-    // {
-        // int index = contentPath.IndexOf("src");
-        // string assetsPath = Path.Combine(contentPath[..index], "static", "assets", file);
-        // if (string.IsNullOrEmpty(assetsPath) | !File.Exists(assetsPath))
-            // return null;
+    private static string? GetJson(string contentPath, string file)
+    {
+        int index = contentPath.IndexOf("src");
+        string assetsPath = Path.Combine(contentPath[..index], "static", "assets", file);
+        if (string.IsNullOrEmpty(assetsPath) | !File.Exists(assetsPath))
+            return null;
 
-        // string json = File.ReadAllText(assetsPath);
-        // if (string.IsNullOrEmpty(json))
-            // return null;
+        string json = File.ReadAllText(assetsPath);
+        if (string.IsNullOrEmpty(json))
+            return null;
 
-        // return json;
-    // }
+        return json;
+    }
 }
 

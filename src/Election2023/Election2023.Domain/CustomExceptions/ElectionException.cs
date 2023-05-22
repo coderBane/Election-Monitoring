89acedd63986230c39cc127e3648ef362203e75e
coderBane <110666273+coderBane@users.noreply.github.com>
2023-05-22 15:33:11 +0100

    edit - update custom exception (election)

--- src/Election2023/Election2023.Domain/CustomExceptions/ElectionException.cs
+++ src/Election2023/Election2023.Domain/CustomExceptions/ElectionException.cs
@@ -1,10 +1,15 @@
+using System.Globalization;
+
 namespace Election2023.Domain.CustomExceptions;
 
 public class ElectionDomainException : Exception
 {
-    public ElectionDomainException() {}
+    public ElectionDomainException() : base() {}
 
     public ElectionDomainException(string message) : base(message) {}
+
+    public ElectionDomainException(string message, params object[] args) 
+        : base(string.Format(CultureInfo.CurrentCulture, message, args)) {}
 
     public ElectionDomainException(string message, Exception innerException) 
         : base(message, innerException) {}
